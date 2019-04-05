06cd39149909d0fe6762116078dc8bd3c486e99c
amandafrois <49296728+amandafrois@users.noreply.github.com>
2019-04-05 00:57:44 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -42,8 +42,11 @@ var x = setInterval(function() {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="demo"
+	
+var pathname = window.location.pathname;
+	
+if(pathname === '/' || pathname === 'index.html'){
+	 // Output the result in an element with id="demo"
   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
     
@@ -53,6 +56,9 @@ var x = setInterval(function() {
     document.getElementById("demo").innerHTML = "Enfim casados!";
   }
 }, 1000);
+}
+ 	
+ 
 
 (function($) {
 
